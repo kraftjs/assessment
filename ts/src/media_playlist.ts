02c563d997c0f2b13b2573918610569f29ee096e
Jason Kraft <developer.kraft@gmail.com>
2023-12-15 11:27:44 -0500

    Create initial static methods to parse file

--- ts/src/media_playlist.ts
+++ ts/src/media_playlist.ts
@@ -13,17 +13,78 @@ export class MediaPlaylist {
   target_duration: MaybeTimeDuration;
   version: number;
 
-  constructor() {
+  constructor(_segments: MediaSegment[], _duration: number, _version: number) {
     this.ended = false;
-    this.segments = [];
-    this.target_duration = Duration.seconds.from(0); // Initialize with the appropriate value
-    this.version = 0;
+    this.segments = _segments;
+    this.target_duration = Duration.seconds.from(_duration); // Initialize with the appropriate value
+    this.version = _version;
   }
 
   // Define the parseExtM3U method
   static parseExtM3U(_file: string): MediaPlaylist {
     // You can implement the parsing logic here
-    return new MediaPlaylist();
+    _file = _file.trim();
+    if (!_file.startsWith('#EXTM3U')) {
+      throw new Error('Input is not a valid .m3u8 file');
+    }
+
+    const segments = this.parseSegments(_file);
+    const duration = this.parseDuration(_file);
+    const version = this.parseVersion(_file);
+
+
+    return new MediaPlaylist(segments, duration, version);
+  }
+
+  private static parseSegments(_file: string): MediaSegment[] {
+    const result = []
+
+    const lines = _file.split(/\r?\n/);
+    for (let [index, line] of lines.entries()) {
+      line = line.trim();
+      if (line.startsWith('#EXTINF') && index + 2 < lines.length) {
+        const [duration, _] = line.split(',');
+        const url = lines[index + 2];
+
+        const segment: MediaSegment = {
+          duration: Duration.seconds.from(Number(duration)),
+          url,
+        }
+        result.push(segment);
+      }
+    }
+
+    return result
+  }
+
+  private static parseVersion(_file: string): number {
+    let version = 0;
+
+    const lines = _file.split(/\r?\n/);
+    for (let line of lines) {
+      line = line.trim();
+      const tag = '#EXT-X-VERSION:'
+      if (line.startsWith(tag)) {
+        version = Number(line.slice(tag.length));
+      }
+    }
+
+    return version;
+  }
+
+  private static parseDuration(_file: string): number {
+    let duration = 0;
+
+    const lines = _file.split(/\r?\n/);
+    for (let line of lines) {
+      line = line.trim();
+      const tag = '#EXT-X-TARGETDURATION:'
+      if (line.startsWith(tag)) {
+        duration = parseInt(line.slice(tag.length));
+      }
+    }
+
+    return duration;
   }
 }
 
